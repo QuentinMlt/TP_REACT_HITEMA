446586a6d8c5062416f20984161d7ad0da7e8495
QuentinMlt <gouss95974>
2021-03-19 13:18:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+/**Import la classe component de react */
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from './Home';
+/**Import les classes BrowserRouter, Route de react-router-dom */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component{
+
+  /**Systeme de routing */
+  render(){
+    return <BrowserRouter>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+
+    </BrowserRouter>
+  }
 }
-
-export default App;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export  default class Home extends Component {
+
+    render() {
+        return <div className="container">
+            <h1>Homepage du TP</h1>
+            <p>Vous pouvez retrouvez les derniers posts publiés</p>
+
+            <h2>Les derniers posts</h2>
+
+        </div>
+    }
+}
